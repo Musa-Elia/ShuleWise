65dede244bb9807ce658e1979a840fb9a3658153
Tamim  Rashid <rashidytamimu@gmail.com>
2024-08-12 13:27:00 +0300

    update

--- server/registerController.php
+++ server/registerController.php
@@ -1,5 +1,15 @@
-<?php 
+<?php  
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require "db.php";
+include('../auth/email-api.php');
+
+$userpatterns = '/^[A-Za-z]+(?:\s[A-Za-z]+)*$/';
+$passwordPattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/';
+
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     if(isset($_POST["register"])){
         // section 1 data 
@@ -13,7 +23,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         $city = mysqli_real_escape_string($connection, $_POST['city']);
         $phone = mysqli_real_escape_string($connection, $_POST['phone']);
 
-        // validation 
+        // check of existing email 
+        $email_check = "SELECT email FROM admin WHERE email = '$email' LIMIT 1";
+        $query_run = mysqli_query($connection, $email_check);
+        if (mysqli_num_rows($query_run) > 0) {
+            $_SESSION['status'] = "Email address already exists";
+            header('Location: ../register.php');
+            exit();
+        } 
         
 
 
